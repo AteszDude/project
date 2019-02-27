3e179457f44b18a251c81dc8aa6a20848b30dd76
Attila <tordaga@gmail.com>
2019-02-27 11:49:14 +0000

    Potential matches

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,33 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   mount_uploader :avatar, AvatarUploader
-  has_many :Match
-  has_many :User, through: :Match
-  
+  has_many :matches
+  has_many :users, through: :matches
   
   #validates_presence_of   :avatar
   #validates_integrity_of  :avatar
   #validates_processing_of :avatar
   
+  #TODO: refactor  
   def getmatches
-    User.find.all
+  @allpersons = []
+  @allmatches = Match.where(person1_id: self.id)
+     @allmatches.each do |t|
+       @allpersons = @allpersons + (User.where(id: t.person2_id))
+     end
+     
+  @allmatches = Match.where(person2_id: self.id)
+     @allmatches.each do |t|
+       @allpersons = @allpersons + (User.where(id: t.person1_id))
+     end
+     
+     return @allpersons
+  end
+
+  def getpotentials
+    @allusers = User.all
+    @allusers = @allusers - getmatches
+    @allusers = @allusers - [self]
   end
   
 end
