d260a0a32d0f96890bcb0e5afed888c70cd8292c
Attila <tordaga@gmail.com>
2019-05-18 12:13:18 +0000

    Experimental sign-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birth_date, :sex, :likes])
+  end
   
   #This redirects the user to the login screen
   #before_action :authenticate_user!
