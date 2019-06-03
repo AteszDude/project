464633fbbb60a7f97c4330614ccfb8f397f593ca
Attila <tordaga@gmail.com>
2019-06-03 19:38:13 +0000

    Frontend sketch for webapp

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -21,6 +21,29 @@ def create
 
 end
 
+
+def getMessages
+ @messages = @conversation.messages
+  if @messages.length > 10
+   @over_ten = true
+   @messages = @messages[-10..-1]
+  end
+  if params[:m]
+   @over_ten = false
+   @messages = @conversation.messages
+  end
+ if @messages.last
+  if @messages.last.user_id != current_user.id
+   @messages.last.read = true;
+  end
+ end
+end
+
+def show
+ render :template => "conversations/index"
+end
+
+
 private
  def conversation_params
   params.permit(:user1_id, :user2_id)
